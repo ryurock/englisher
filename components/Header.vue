54dc874ee5c4da378c79e77e2499ccd5296a4fa8
ryurock <ryusukekimura3@gmail.com>
2018-08-30 06:37:49 +0900

    トップページを微調整

---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <h1 class="logo">enner</h1>
+  </header>
+</template>
+<style>
+.header {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  background-color: #3b8070;
+}
+.logo {
+  width: 10vh;
+  height: 5vh;
+  margin: 0;
+  margin-left: 1vh;
+  font-size: 22px;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-style: italic;
+  font-weight: normal;
+  line-height: 5vh;
+  color: white;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <header class="header">
-      <h1 class="logo">enner</h1>
-    </header>
+    <AppHeader />
     <nuxt/>
   </div>
 </template>
@@ -24,35 +22,6 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-
 .header {
   position: absolute;
   top: 0;
@@ -72,4 +41,11 @@ html {
   color: white;
 }
 </style>
+<script>
+import AppHeader from "~/components/Header";
+export default {
+  components: {AppHeader}
+}
+</script>
+
 
