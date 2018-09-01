7f204023cbee993af385952f7dcc671f70bec423
ryurock <ryusukekimura3@gmail.com>
2018-09-02 02:04:38 +0900

    トップページのリンクを修正

--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,40 @@
         英単語を素早く覚えましょう
       </h1>
       <h2 class="subtitle">
-        ennerはベーシック英語を元にしたシンプルな英単語、フレーズを学べます。
+        ennerはベーシック英語やスペシャルイングリッシュを元にしたシンプルな英単語、フレーズを学べます。
       </h2>
       <div class="links">
-        <a
-          href="/speedcard/basic-english/850"
-          target="_blank"
-          class="button--green">スピードカードで英単語を覚える</a>
+        <nuxt-link to="/speedcard/basic-english/850/fast" class="button--green">
+        スピードカードで英単語を覚える
+        </nuxt-link>
       </div>
     </div>
+
+    <section class="sub-section">
+      <h3>スピードカードとは？</h3>
+      <p class="text-center">英単語カードのように単語やフレーズをめくりながら黙読できる機能です。</p>
+      <div class="links">
+        <nuxt-link to="/speedcard/basic-english/850/veryfast" class="button--green">
+        スピードカードで英単語を覚える(2倍速)
+        </nuxt-link>
+      </div>
+      <div class="links">
+        <nuxt-link to="/speedcard/basic-english/850/fast" class="button--green">
+        スピードカードで英単語を覚える(1.5倍速)
+        </nuxt-link>
+      </div>
+      <div class="links">
+        <nuxt-link to="/speedcard/basic-english/850/normal" class="button--green">
+        スピードカードで英単語を覚える(1倍速)
+        </nuxt-link>
+      </div>
+      <div class="links">
+        <nuxt-link to="/speedcard/basic-english/850/fast" class="button--green">
+        スピードカードで英単語を覚える(低速)
+        </nuxt-link>
+      </div>
+    </section>
+
     <section class="sub-section">
       <h3>ベーシック英語とは？</h3>
       <p>基本単語が850と少ない単語とルールで成り立つ英語の事です。ennerでは辞書のような冗長的な品詞の種類等を取り除いて見やすく覚えやすいです。</p>
@@ -24,6 +49,21 @@
           class="button--green">ベーシック英単語一覧を見る</a>
       </div>
     </section>
+
+    <section class="sub-section">
+      <h3>スペシャルイングリッシュとは？</h3>
+      <p>
+        英語を母国語としない人向けに単語を約1500語に絞ったアメリカ合衆国政府が運営する国営放送<a href="https://www.voanews.com/" target="_blank">Voice of America</a>で使われている英語のことです。
+        簡易な文法で非ネイティブの人がアメリカ英語を勉強するのに人気があります。
+      </p>
+      <div class="links">
+        <a
+          href="/speedcard/basic-english/850"
+          target="_blank"
+          class="button--green">スペシャルイングリッシュの単語一覧を見る</a>
+      </div>
+    </section>
+
   </main>
 </template>
 
@@ -42,6 +82,10 @@ export default {
 </script>
 
 <style>
+a {
+  color: #3b8070;
+  text-decoration: none;
+}
 .container {
   color: #526488;
   width: 90%;
@@ -116,12 +160,15 @@ export default {
   text-align: justify;
 }
 
+
 @media screen and (min-width: 760px) {
   .subtitle {
+    text-align: center;
     margin: auto 10%;
   }
   .sub-section p {
-    margin: auto 10%;
+    text-align: center;
+    margin: auto 15%;
   }
 }
 </style>
