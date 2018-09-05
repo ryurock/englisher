12d8df36781e08e2ace813e5cb0e279183ca7ada
ryurock <ryusukekimura3@gmail.com>
2018-09-05 23:10:34 +0900

    ディレクトリを移動

--- batches/hoge.js
+++ batches/hoge.js
@@ -7,25 +7,6 @@ const nlp = require('compromise');
 glob('./dataset/special-english/*.json', (err, files) => {
   files.forEach((file, index) => {
     const dict = require(`../${file}`);
-    let data = {
-      id: (index + 1),
-      tokens: dict.tokens,
-      token: [],
-      sort: dict.tokens[0].substr(0, 1),
-      meta: dict.meta,
-      phrases: [],
-      translations: dict.translations
-    };
-
-    const tokens = dict.tokens[0].split(' ');
-    for (let i = 0; i < tokens.length; i++) {
-      let token = {
-        text: tokens[i],
-        postTags: nlp(tokens[i]).terms(0).data()[0].tags,
-        bestPosTag: nlp(tokens[i]).terms(0).data()[0].bestTag
-      };
-      data.token.push(token);
-    }
-    fs.writeFileSync(`./dataset/special-english/${dict.tokens[0]}.json`, JSON.stringify(data, null, '  '));
+    fs.writeFileSync(`./models/words/special-english/raw/${dict.tokens[0]}.json`, JSON.stringify(dict, null, '  '));
   });
 });
