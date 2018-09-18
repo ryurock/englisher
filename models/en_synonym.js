3096e3e2b63c11f455463a25910dc25a5a5fd749
ryurock <ryusukekimura3@gmail.com>
2018-09-18 21:07:58 +0900

    関連語句modelを追加

---
+++ models/en_synonym.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const en_synonym = sequelize.define('en_synonym', {
+    en_word_id: DataTypes.INTEGER
+  }, {
+    underscored: true,
+  });
+  en_synonym.associate = function(models) {
+    // associations can be defined here
+  };
+  return en_synonym;
+};
