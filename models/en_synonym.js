2ba078bda7e1d3218e111945d96ff83543fbdd40
ryurock <ryusukekimura3@gmail.com>
2018-09-21 20:59:32 +0900

    DBデータをファイルに変換

--- models/en_synonym.js
+++ models/en_synonym.js
@@ -9,5 +9,16 @@ module.exports = (sequelize, DataTypes) => {
   en_synonym.associate = function(models) {
     // associations can be defined here
   };
+
+  en_synonym.findBySynonymWords = function(enWordId) {
+    return sequelize.query(`
+    SELECT
+      es.en_word_id, es.synonym_word_id, ew.token
+    FROM en_synonyms es
+    INNER JOIN en_words ew ON ew.id = es.synonym_word_id
+    WHERE es.en_word_id = ?
+    `,
+    { replacements: [ enWordId ], type: sequelize.QueryTypes.SELECT });
+  };
   return en_synonym;
 };
--- models/en_word.js
+++ models/en_word.js
@@ -1,12 +1,27 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   const en_word = sequelize.define('en_word', {
     token: DataTypes.STRING
   }, {
     underscored: true,
   });
+
   en_word.associate = function(models) {
-    // associations can be defined here
+    en_word.hasMany(models.en_meta_tag);
   };
+
+  en_word.findMetaTag = function(enWordId){
+    return sequelize.query(`
+    SELECT
+      ew.id,ew.token,ew.created_at,ew.updated_at,
+      emt.meta_key,emt.meta_value
+    FROM en_words ew
+    INNER JOIN en_meta_tags emt on emt.en_word_id = ew.id
+    WHERE ew.id = ?
+    `,
+    { replacements: [enWordId], type: sequelize.QueryTypes.SELECT });
+  };
+
   return en_word;
 };
