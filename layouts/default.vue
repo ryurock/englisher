a027b3667736237a382bff2cfc5d247df3caef97
ryurock <ryusukekimura3@gmail.com>
2018-09-27 23:17:10 +0900

    サイトオープン用に調整する

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,10 @@
     <nuxt/>
   </div>
 </template>
+<style lang="scss">
+@import '@/assets/scss/_base.scss';
+
+</style>
 <script>
 import AppHeader from "~/components/AppHeader";
 export default {
