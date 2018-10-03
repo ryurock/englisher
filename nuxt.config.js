612b0e1ab7383a5b428f63006bc8f73430440dbc
ryurock <ryusukekimura3@gmail.com>
2018-10-04 00:01:44 +0900

    generateに英単語記事詳細を追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,10 @@
 module.exports = {
   generate: {
     routes: function(callback) {
-      // const basicEnglish = require('./datasets/words/basic-english/speedcard.min.json');
-      // let routeMap = basicEnglish.map(((word) => {
-      //   return `/word/basic-english/${word.token}`;
-      // }));
-
-      // const specialEnglish = require('./datasets/words/special-english/speedcard.min.json');
-      // let specialEnglishWords = specialEnglish.map((word) => {
-      //   return `/word/special-english/${word.token}`;
-      // });
-      // routeMap = routeMap.concat(specialEnglishWords);
+      const basicEnglish = require('./datasets/words/basic-english/activate.json');
+      let routeMap = basicEnglish.activate.map(((word) => {
+        return `/word/basic-english/${word}`;
+      }));
 
       const speedCardBasicEnglishPages = [
         '/speedcard/basic-english/veryfast',
@@ -24,7 +18,7 @@ module.exports = {
         '/speedcard/special-english/normal',
         '/speedcard/special-english/slow',
       ];
-      let routeMap = [].concat(speedCardBasicEnglishPages).concat(speedCardSpecialEnglishPages);
+      routeMap = routeMap.concat(speedCardBasicEnglishPages).concat(speedCardSpecialEnglishPages);
       callback(null, routeMap);
     }
   },
